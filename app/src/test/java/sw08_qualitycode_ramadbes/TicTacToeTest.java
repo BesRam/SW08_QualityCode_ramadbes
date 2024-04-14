0c56674d6bfb7b51dfac909d7f07e50d326f5c5c
Besfort Ramadani <71851042+BesRam@users.noreply.github.com>
2024-04-14 21:14:19 +0200

    add testing code to check if player X always starts

--- app/src/test/java/sw08_qualitycode_ramadbes/TicTacToeTest.java
+++ app/src/test/java/sw08_qualitycode_ramadbes/TicTacToeTest.java
@@ -1,5 +1,6 @@
 package sw08_qualitycode_ramadbes;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -9,5 +10,11 @@ public class TicTacToeTest {
     void testInitialBoardIsEmpty() {
         TicTacToe game = new TicTacToe();
         assertTrue(game.isBoardEmpty(), "The board should be empty at the beginning of the game.");
+    }
+
+    @Test
+    void testPlayerXGoesFirst() {
+        TicTacToe game = new TicTacToe();
+        assertEquals('X', game.getCurrentPlayer(), "Player X should start the game.");
     }
 }
